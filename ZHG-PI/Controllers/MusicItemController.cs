ead7db31121ac54e6cebd6acf290f2686b9995b6
Zhicong HUANG <zhicong.huang@exakis-nelite.com>
2023-02-20 14:28:07 +0100

    add-MusicItemController

--- ZHG-PI/Controllers/MusicItemController.cs
+++ ZHG-PI/Controllers/MusicItemController.cs
@@ -1,33 +1,38 @@
 using Azure.Data.Tables;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using ZHG_PI.Models;
+
 
 namespace ZHG_PI.Controllers
 {
     [ApiController]
-    [EnableCors]
     [Route("musicitems")]
     public class MusicItemController : ControllerBase
     {
-        private readonly ILogger<MusicItemController> _logger;
 
-        private readonly TableServiceClient _tableServiceClient;
+        const string connectionString = "DefaultEndpointsProtocol=https;AccountName=zhgpistorage;AccountKey=Gvq4stQSvsFnHk7Rl7oohl7bzxOhh8FocHFmxTwSiOOU2aWS9U2RfeZbrIqnolUo6Qeic/1v1CqM+AStrfcKQQ==;EndpointSuffix=core.windows.net";
+        const string tableName = "musicItemsTable";
 
-        public MusicItemController(ILogger<MusicItemController> logger, TableServiceClient tableServiceClient)
-        {
-            _tableServiceClient = tableServiceClient;
-            _logger = logger;
-        }
+        public MusicItemController(){}
 
         // get all music item
         [HttpGet]
         public IEnumerable<MusicItem> GetAll()
         {
-            var tableClient = _tableServiceClient.GetTableClient("StudentTable");
+            var tableClient = new TableClient(connectionString, tableName);
             var response = tableClient.Query<MusicItem>();
             return response.ToList();
         }
+
+        // get music item by rowkey(id)
+        [HttpGet("{id}")]
+        public MusicItem Get(string id)
+        {
+            var tableClient = new TableClient(connectionString, tableName);
+            var response = tableClient.GetEntity<MusicItem>("Solo", id);
+            return response.Value;
+        }
+
+
     }
 }
--- ZHG-PI/Program.cs
+++ ZHG-PI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Azure;
+using Azure.Data.Tables;
+using ZHG_PI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["ZHG-PI-Connection:blob"], preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["ZHG-PI-Connection:queue"], preferMsi: true);
+});
+
+
+
+// const string connectionString = "DefaultEndpointsProtocol=https;AccountName=zhgpistorage;AccountKey=Gvq4stQSvsFnHk7Rl7oohl7bzxOhh8FocHFmxTwSiOOU2aWS9U2RfeZbrIqnolUo6Qeic/1v1CqM+AStrfcKQQ==;EndpointSuffix=core.windows.net";
+// const string tableName = "musicItemsTable";
+
+
+// // Construct a new TableClient using a connection string.
+// var tableClient = new TableClient(connectionString, tableName);
+// // Create the table if it doesn't already exist to verify we've successfully authenticated.
+// await tableClient.CreateIfNotExistsAsync();
+
+
+// var tableEntities = TableEntities.allTableEntities();
+
+// foreach (var item in tableEntities)
+// {
+//     // Entity doesn't exist in table, so invoking UpsertEntity will simply insert the entity.
+//     await tableClient.UpsertEntityAsync(item);
+// }
+
+
 
 var app = builder.Build();
 
